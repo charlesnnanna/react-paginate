c90e0638ec58b9290057474efd8ff40de4e5656a
charlesnnanna <charlesnnanna1667@gmail.com>
2022-04-14 10:42:40 -0700

    Added pagination with react-paginate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,86 @@
-import Axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react'
+import ReactPaginate from 'react-paginate';
 
+class App extends Component{
+  constructor(props) {
 
-function App() {
-  return (
-    <div>
-      <h1>A test react App</h1>
-    </div>
-  )
+    super(props);
+
+    this.state = {
+      offset : 0,
+      data : [],
+      perPage : 10,
+      currentPage : 0
+    }
+  }
+
+  receivedData(){
+    axios.get(`https://jsonplaceholder.typicode.com/photos`)
+    .then((response) => {
+      const data = response.data
+      this.setState ({
+        data : data
+      })
+      console.log(this.state.data)
+      this.displayDataOnScreen()
+    })
+  }
+
+  displayDataOnScreen = () => {
+    const slice = this.state.data.slice(this.state.offset, this.state.offset + this.state.perPage)
+    console.log(slice)
+      const postData = slice.map ((item, index) => {
+        return(
+          <React.Fragment key = {index}>
+          <p>{item.title}</p>
+          <img src = {item.thumbnailUrl} alt = {item.title} />
+        </React.Fragment>
+        )  
+      })
+
+      this.setState({
+        pageCount : Math.ceil(this.state.data.length / this.state.perPage),
+        postData 
+      })
+  }
+
+  handlePageClick = (event) => {
+    const selectedPage = event.selected
+    const offset = selectedPage * this.state.perPage
+
+    this.setState ({
+      offset : offset,
+      currentPage : selectedPage
+    })
+
+    this.displayDataOnScreen()
+  }
+
+  componentDidMount () {
+    this.receivedData()
+  }
+
+  render(){
+
+    return(
+        <>
+          {this.state.postData}
+          <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+        </>
+    )
+  }
 }
 
-export default App;
+export default App
